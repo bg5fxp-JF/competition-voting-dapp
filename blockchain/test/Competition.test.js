bfd43b0dc6fabdcce167adebee5c2e688cbcdc67
bg5fxp-JF <jacobfecunda@outlook.com>
2023-11-25 11:52:24 +0000

    added selectJudges tests

--- blockchain/test/Competition.test.js
+++ blockchain/test/Competition.test.js
@@ -43,6 +43,94 @@ describe("Competition Unit Tests", function () {
 			).to.be.revertedWithCustomError(competition_account5, "NotApproved");
 		});
 
-		it("should revert if the judges length doesn't meet requirements", async function () {});
+		it("should revert if the judges length doesn't meet requirements", async function () {
+			const judges = [
+				account1.address,
+				account2.address,
+				account3.address,
+				account4.address,
+			];
+			await expect(
+				competition.selectJudges(judges)
+			).to.be.revertedWithCustomError(competition, "TooBig");
+			await expect(competition.selectJudges([])).to.be.revertedWithCustomError(
+				competition,
+				"NotEnough"
+			);
+		});
+
+		it("should revert if one of the judges has approved(owner) capabilities", async function () {
+			const judges = [account1.address, account2.address, account3.address];
+			const judges2 = [deployer.address, account2.address, account3.address];
+
+			const competition_account1 = await competition.connect(account1);
+			await competition_account1.getCapabilites();
+
+			await expect(
+				competition.selectJudges(judges)
+			).to.be.revertedWithCustomError(competition, "AlreadyApproved");
+			await expect(
+				competition.selectJudges(judges2)
+			).to.be.revertedWithCustomError(competition, "AlreadyOwner");
+		});
+
+		it("should be reverted if one of judges is already a finalist", async function () {
+			const judges = [account1.address, account2.address, account3.address];
+			const finalists = [account3.address, account4.address];
+
+			await competition.selectFinalists(finalists);
+
+			await expect(
+				competition.selectJudges(judges)
+			).to.be.revertedWithCustomError(competition, "AlreadyFinalist");
+		});
+
+		it("should successfully update voting status (owner)", async function () {
+			const expectedStatus = [
+				true /* hasSelectedJudges */,
+				false /* hasInputWeight*/,
+				false /* hasSelectedFinalists */,
+				false /* hasStartedVoting */,
+			];
+			const judges = [account1.address, account2.address, account3.address];
+
+			await competition.selectJudges(judges);
+			assert.equal(
+				await competition.getVotingStatus(),
+				expectedStatus.toString()
+			);
+		});
+		it("should successfully update voting status (approved sender)", async function () {
+			const expectedStatus = [
+				true /* hasSelectedJudges */,
+				false /* hasInputWeight*/,
+				false /* hasSelectedFinalists */,
+				false /* hasStartedVoting */,
+			];
+			const judges = [account2.address, account3.address];
+
+			const competition_account1 = await competition.connect(account1);
+			await competition_account1.getCapabilites();
+
+			await competition_account1.selectJudges(judges);
+			assert.equal(
+				await competition_account1.getVotingStatus(),
+				expectedStatus.toString()
+			);
+		});
+		it("should revert if started the voting", async function () {
+			const judges = [account1.address, account2.address];
+			const finalists = [account3.address, account4.address];
+
+			await competition.selectJudges(judges);
+			await competition.selectFinalists(finalists);
+			await competition.inputWeightage(1, 1);
+
+			await competition.startVoting();
+
+			await expect(
+				competition.selectJudges(judges)
+			).to.be.revertedWithCustomError(competition, "VotingAlreadyStarted");
+		});
 	});
 });
