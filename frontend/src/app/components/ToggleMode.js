19d586d0f1ef9efd38f70dd5f81efea7b88e7118
bg5fxp-JF <jacobfecunda@outlook.com>
2023-12-03 15:32:40 +0000

    changed toggle

--- frontend/src/app/components/ToggleMode.js
+++ frontend/src/app/components/ToggleMode.js
@@ -1,6 +1,7 @@
 "use client";
 
 import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
+import { AnimatePresence, motion } from "framer-motion";
 import { useTheme } from "next-themes";
 
 import {
@@ -9,32 +10,78 @@ import {
 	DropdownMenuItem,
 	DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { useState } from "react";
 
 export default function ToggleMode() {
 	const { setTheme } = useTheme();
+	const [isOpen, setIsOpen] = useState(false);
 
 	return (
-		<div className="fixed bottom-10 right-6 sm:right-16 sm:text-reg">
-			<DropdownMenu>
-				<DropdownMenuTrigger asChild>
-					<button className="p-5 rounded text-sm bg-primaryColor dark:bg-primaryColor/70 shadow-md ">
-						<SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:hidden" />
-						<MoonIcon className="h-[1.2rem] w-[1.2rem] hidden transition-all dark:block dark:rotate-0 dark:scale-100" />
-						<span className="sr-only">Toggle theme</span>
-					</button>
-				</DropdownMenuTrigger>
-				<DropdownMenuContent align="end">
-					<DropdownMenuItem onClick={() => setTheme("light")}>
-						Light
-					</DropdownMenuItem>
-					<DropdownMenuItem onClick={() => setTheme("dark")}>
-						Dark
-					</DropdownMenuItem>
-					<DropdownMenuItem onClick={() => setTheme("system")}>
-						System
-					</DropdownMenuItem>
-				</DropdownMenuContent>
-			</DropdownMenu>
-		</div>
+		<motion.div
+			initial={{ opacity: 0 }}
+			animate={{ opacity: 1 }}
+			exit={{ opacity: 0 }}
+			className="fixed   bottom-10 right-6 p-5 rounded text-sm bg-primaryColor dark:bg-primaryColor/70 shadow-md transition-transform sm:right-16 sm:text-reg"
+		>
+			{isOpen ? (
+				<div className="p-1">
+					<motion.div
+						initial={{ opacity: 0, y: -10 }}
+						animate={{ opacity: 1, y: 0 }}
+						exit={{ opacity: 0 }}
+						className="flex  gap-x-3 "
+					>
+						<button
+							onClick={() => {
+								setTheme("light");
+								setIsOpen(!isOpen);
+							}}
+							className="bg-secondaryColor dark:bg-primaryColor/20 p-2 rounded transition-all active:scale-125"
+						>
+							Light
+						</button>
+						<button
+							onClick={() => {
+								setTheme("dark");
+								setIsOpen(!isOpen);
+							}}
+							className="bg-secondaryColor dark:bg-primaryColor/20 p-2 rounded transition-all active:scale-125"
+						>
+							Dark
+						</button>
+						<button
+							onClick={() => {
+								setTheme("system");
+								setIsOpen(!isOpen);
+							}}
+							className="bg-secondaryColor dark:bg-primaryColor/20 p-2 rounded transition-all active:scale-125"
+						>
+							System
+						</button>
+					</motion.div>
+				</div>
+			) : (
+				<AnimatePresence>
+					<motion.div
+						initial={{ opacity: 0, y: 10 }}
+						animate={{ opacity: 1, y: 0 }}
+						exit={{ opacity: 0 }}
+					>
+						<SunIcon
+							className="cursor-pointer rotate-0 scale-100 transition-all dark:hidden"
+							onClick={() => {
+								setIsOpen(!isOpen);
+							}}
+						/>
+						<MoonIcon
+							className=" cursor-pointer hidden transition-all dark:block dark:rotate-0 dark:scale-100"
+							onClick={() => {
+								setIsOpen(!isOpen);
+							}}
+						/>
+					</motion.div>
+				</AnimatePresence>
+			)}
+		</motion.div>
 	);
 }
