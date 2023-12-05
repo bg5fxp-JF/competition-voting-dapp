922cd9904e79adb04f8dbc51df486438a85d1177
bg5fxp-JF <jacobfecunda@outlook.com>
2023-12-05 13:34:41 +0000

    functionality for submitting judges

--- frontend/src/app/set-competition/components/JudgesInput.js
+++ frontend/src/app/set-competition/components/JudgesInput.js
@@ -1,9 +1,39 @@
 "use client";
 import { useState } from "react";
 import { MinusIcon, PlusIcon } from "@radix-ui/react-icons";
+import { useAccount, useContractWrite, useNetwork } from "wagmi";
+import { abi, contractAddresses } from "@/app/constants";
+import { useToast } from "@/components/ui/use-toast";
 
 export default function JudgesInput({ isDisabled }) {
 	const [judges, setJudges] = useState([""]);
+	const { toast } = useToast();
+
+	const { chain } = useNetwork();
+	const { isConnected } = useAccount();
+
+	const chainId = isConnected ? chain.id : 0;
+	const competitionAddress =
+		chainId in contractAddresses ? contractAddresses[chainId][0] : null;
+
+	const { write } = useContractWrite({
+		address: competitionAddress,
+		abi: abi,
+		functionName: "selectJudges",
+		args: [judges],
+		onError(error) {
+			toast({
+				variant: "destructive",
+				title: "Uh oh! Something went wrong.",
+				description: error.message,
+			});
+		},
+		onSuccess() {
+			toast({
+				title: "Successfuly Selected Judges",
+			});
+		},
+	});
 
 	function handleJudgeAdd() {
 		setJudges([...judges, ""]);
@@ -20,19 +50,22 @@ export default function JudgesInput({ isDisabled }) {
 		list[index] = value;
 		setJudges(list);
 	}
+
 	return (
 		<div className="flex flex-col gap-4  transition-all">
 			{judges.map((judge, index) => {
 				return (
 					<div key={index} className="flex gap-x-3 ">
 						<div
-							className={`p-2 rounded shadow-md w-full ${
-								isDisabled && "bg-[#fafafa]"
+							className={`p-2 rounded shadow-md w-full dark:bg-gray-700 ${
+								isDisabled && "bg-[#fafafa] dark:bg-gray-800"
 							} `}
 						>
 							<input
 								placeholder="Enter an address"
-								className=" focus-visible:outline-none "
+								className={` dark:bg-gray-700 focus-visible:outline-none w-full ${
+									isDisabled && "bg-[#fafafa] dark:bg-gray-800 "
+								} `}
 								disabled={isDisabled}
 								type="text"
 								required
@@ -53,12 +86,23 @@ export default function JudgesInput({ isDisabled }) {
 				);
 			})}
 			{judges.length < 3 && !isDisabled && (
-				<button
-					onClick={() => handleJudgeAdd()}
-					className="flex p-2  justify-center rounded  bg-primaryColor dark:bg-primaryColor/70 shadow-md "
-				>
-					<PlusIcon />
-				</button>
+				<div className="flex gap-x-2">
+					<button
+						onClick={() => handleJudgeAdd()}
+						className="flex w-full p-2 justify-center rounded  bg-primaryColor dark:bg-primaryColor/70 shadow-md "
+					>
+						<PlusIcon width={20} height={20} />
+					</button>
+
+					<button
+						onClick={() => write()}
+						className={`flex p-2 w-full  justify-center text-sm  rounded  bg-primaryColor dark:bg-primaryColor/70 shadow-md transition-all ${
+							judges[0].length == 42 ? "" : "hidden"
+						} `}
+					>
+						Submit
+					</button>
+				</div>
 			)}
 		</div>
 	);
