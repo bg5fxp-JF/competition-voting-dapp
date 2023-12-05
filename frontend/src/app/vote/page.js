cc697f222b5ea3bbc8337bc274be564170a18112
bg5fxp-JF <jacobfecunda@outlook.com>
2023-12-05 13:35:05 +0000

    changed style

--- frontend/src/app/vote/page.js
+++ frontend/src/app/vote/page.js
@@ -3,7 +3,7 @@ import FinalistCard from "./components/FinalistCard";
 
 export default function page() {
 	return (
-		<div className="flex items-center justify-center mt-36 mb-24">
+		<div className="flex items-center justify-center mt-44 mb-24">
 			<div className="flex flex-col gap-5 items-center">
 				<h4 className="text-lg">The Finalists Are...</h4>
 				<div className="flex flex-wrap w-full justify-center mx-auto px-6 gap-4 sm:px-0">
