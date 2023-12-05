b48d073d1a675991a792a253af9d2123c26deaf8
bg5fxp-JF <jacobfecunda@outlook.com>
2023-12-05 13:34:50 +0000

    functionality for submitting weights

--- frontend/src/app/set-competition/components/WeightageInput.js
+++ frontend/src/app/set-competition/components/WeightageInput.js
@@ -1,17 +1,51 @@
 "use client";
+import { abi, contractAddresses } from "@/app/constants";
+import { useToast } from "@/components/ui/use-toast";
 import { useState } from "react";
+import { useAccount, useContractWrite, useNetwork } from "wagmi";
 
 export default function WeightageInput({ isDisabled }) {
 	const [judgeWeight, setJudgeWeight] = useState(0);
-	const [finalistWeight, setFinalistWeight] = useState(0);
+	const [audienceWeight, setAudienceWeight] = useState(0);
+	const { toast } = useToast();
+
+	const { chain } = useNetwork();
+	const { isConnected } = useAccount();
+
+	const chainId = isConnected ? chain.id : 0;
+	const competitionAddress =
+		chainId in contractAddresses ? contractAddresses[chainId][0] : null;
+
+	const { write } = useContractWrite({
+		address: competitionAddress,
+		abi: abi,
+		functionName: "inputWeightage",
+		args: [judgeWeight, audienceWeight],
+		onError(error) {
+			toast({
+				variant: "destructive",
+				title: "Uh oh! Something went wrong.",
+				description: error.message,
+			});
+		},
+		onSuccess() {
+			toast({
+				title: "Successfuly Selected Weightages",
+			});
+		},
+	});
 
 	return (
 		<div className="flex flex-col gap-3">
-			<div className={`p-2 rounded shadow-md ${isDisabled && "bg-[#fafafa]"} `}>
+			<div
+				className={`p-2 rounded shadow-md dark:bg-gray-700 ${
+					isDisabled && "bg-[#fafafa] dark:bg-gray-800"
+				} `}
+			>
 				<input
 					placeholder="Enter Judge Weight"
-					className={` focus-visible:outline-none ${
-						isDisabled && "bg-[#fafafa]"
+					className={` dark:bg-gray-700 focus-visible:outline-none w-full ${
+						isDisabled && "bg-[#fafafa] dark:bg-gray-800 "
 					} `}
 					type="number"
 					disabled={isDisabled}
@@ -22,21 +56,33 @@ export default function WeightageInput({ isDisabled }) {
 					}}
 				/>
 			</div>
-			<div className={`p-2 rounded shadow-md ${isDisabled && "bg-[#fafafa]"} `}>
+			<div
+				className={`p-2 rounded shadow-md dark:bg-gray-700 ${
+					isDisabled && "bg-[#fafafa] dark:bg-gray-800"
+				} `}
+			>
 				<input
-					placeholder="Enter Finalist Weight"
-					className={` focus-visible:outline-none ${
-						isDisabled && "bg-[#fafafa]"
+					placeholder="Enter Audience Weight"
+					className={` dark:bg-gray-700 focus-visible:outline-none w-full ${
+						isDisabled && "bg-[#fafafa] dark:bg-gray-800 "
 					} `}
 					disabled={isDisabled}
 					type="number"
 					min={0.1}
 					required
 					onChange={(e) => {
-						setFinalistWeight(e.target.value);
+						setAudienceWeight(e.target.value);
 					}}
 				/>
 			</div>
+			{judgeWeight > 0 && audienceWeight > 0 && (
+				<button
+					onClick={() => write()}
+					className="flex p-2 w-full  justify-center text-sm  rounded  bg-primaryColor dark:bg-primaryColor/70 shadow-md transition-all"
+				>
+					Submit
+				</button>
+			)}
 		</div>
 	);
 }
