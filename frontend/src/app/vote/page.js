e825dc9ddba357fae956be4d664bda561207d43e
bg5fxp-JF <jacobfecunda@outlook.com>
2023-12-06 10:40:11 +0000

    update vote page

--- frontend/src/app/vote/page.js
+++ frontend/src/app/vote/page.js
@@ -1,19 +1,11 @@
 import React from "react";
-import FinalistCard from "./components/FinalistCard";
+
+import Finalists from "./components/Finalists";
 
 export default function page() {
 	return (
 		<div className="flex items-center justify-center mt-44 mb-24">
-			<div className="flex flex-col gap-5 items-center">
-				<h4 className="text-lg">The Finalists Are...</h4>
-				<div className="flex flex-wrap w-full justify-center mx-auto px-6 gap-4 sm:px-0">
-					<FinalistCard img="https://avatars.githubusercontent.com/u/52407573?v=4" />
-					<FinalistCard img="https://avatars.githubusercontent.com/u/52407573?v=4" />
-					<FinalistCard img="https://avatars.githubusercontent.com/u/52407573?v=4" />
-					<FinalistCard img="https://avatars.githubusercontent.com/u/52407573?v=4" />
-					<FinalistCard img="https://avatars.githubusercontent.com/u/52407573?v=4" />
-				</div>
-			</div>
+			<Finalists />
 		</div>
 	);
 }
