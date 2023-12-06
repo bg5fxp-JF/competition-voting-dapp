496a9759239fbb0fe65ffcb3e7f78e1da24b3c12
bg5fxp-JF <jacobfecunda@outlook.com>
2023-12-06 13:56:10 +0000

    added toast for get capability

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -8,13 +8,15 @@ import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { useApprovedData } from "./context/ApprovedContext";
 import { abi, contractAddresses } from "./constants";
+import { useToast } from "@/components/ui/use-toast";
 
 export default function Home() {
 	const { connect } = useConnect({
 		connector: new MetaMaskConnector(),
 	});
+	const { toast } = useToast();
 
-	const { isApproved } = useApprovedData();
+	const { isApproved, setIsApproved } = useApprovedData();
 	const { isConnected } = useAccount();
 	const { chain } = useNetwork();
 
@@ -28,6 +30,20 @@ export default function Home() {
 		address: competitionAddress,
 		abi: abi,
 		functionName: "getCapabilites",
+		onError(error) {
+			toast({
+				variant: "destructive",
+				title: "Uh oh! Something went wrong.",
+				description: error.message,
+			});
+		},
+		onSuccess() {
+			toast({
+				title: "Successfuly Got Capabilities",
+			});
+			setIsApproved(true);
+			router.push("/set-competition");
+		},
 	});
 
 	useEffect(() => {
