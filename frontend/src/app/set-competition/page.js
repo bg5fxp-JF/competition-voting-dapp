c2fd7a563bcd8d70e9ae864e8968711481621eb6
bg5fxp-JF <jacobfecunda@outlook.com>
2023-12-06 10:39:46 +0000

    updated reading values from contract

--- frontend/src/app/set-competition/page.js
+++ frontend/src/app/set-competition/page.js
@@ -8,12 +8,27 @@ import { useApprovedData } from "../context/ApprovedContext";
 import { useRouter } from "next/navigation";
 import StartButton from "./components/StartButton";
 import EndButton from "./components/EndButton";
+import { useAccount, useContractRead, useNetwork } from "wagmi";
+import { abi, contractAddresses } from "../constants";
 
 export default function page() {
-	const IS_DISABLED = false;
 	const { isApproved } = useApprovedData();
-
 	const router = useRouter();
+
+	const { chain } = useNetwork();
+	const { isConnected } = useAccount();
+
+	const chainId = isConnected ? chain.id : 0;
+	const competitionAddress =
+		chainId in contractAddresses ? contractAddresses[chainId][0] : null;
+
+	const isDisabled = useContractRead({
+		address: competitionAddress,
+		abi: abi,
+		functionName: "getVotingStatus",
+		watch: true,
+	}).data;
+
 	useEffect(() => {
 		if (!isApproved) {
 			router.push("/");
@@ -23,20 +38,20 @@ export default function page() {
 		<div className="flex flex-col gap-7  justify-center mt-44 mb-24 px-6 sm:px-16 ">
 			<div className="flex flex-col gap-7 justify-center sm:flex-row">
 				<div className="flex flex-col gap-y-7">
-					<h3 className="-mb-3">Select Judges</h3>
-					<JudgesInput isDisabled={IS_DISABLED} />
+					<h3 className="-mb-3 font-bold">Select Judges</h3>
+					<JudgesInput isDisabled={isDisabled} />
 
-					<h3 className="-mb-3">Select Finalists</h3>
-					<FinalistInput isDisabled={IS_DISABLED} />
+					<h3 className="-mb-3 font-bold">Select Finalists</h3>
+					<FinalistInput isDisabled={isDisabled} />
 				</div>
 				<div className="flex flex-col gap-y-7">
-					<h3 className="-mb-3">Select Weightages</h3>
-					<WeightageInput isDisabled={IS_DISABLED} />
+					<h3 className="-mb-3 font-bold">Select Weightages</h3>
+					<WeightageInput isDisabled={isDisabled} />
 				</div>
 			</div>
-			<StartButton isDisabled={IS_DISABLED} />
+			<StartButton isDisabled={isDisabled} />
 
-			<EndButton isDisabled={!IS_DISABLED} />
+			<EndButton isDisabled={!isDisabled} />
 		</div>
 	);
 }
